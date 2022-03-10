14eb917edd8f25c297a8dd6f24bc511c28de2b86
Jay Gadariya <jaygadariya7397@gmail.com>
2022-03-10 17:22:47 +0530

    adjusted code for flutter web

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -31,140 +31,207 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  Size? size;
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        elevation: 0.0,
-        automaticallyImplyLeading: false,
-        centerTitle: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              onTap: () => Get.back(),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.arrow_back_ios_rounded,
-                    color: Theme.of(context).primaryColor,
-                    size: 35.0,
-                  ),
-                  Text(
-                    " Back",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
+    size = MediaQuery.of(context).size;
+    final double itemHeight = (size!.height) / matrixLength.value;
+    final double itemWidth = size!.width / matrixLength.value;
+    return WillPopScope(
+      onWillPop: () {
+        Get.back();
+        return Future(() => true);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0.0,
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          title: kIsWeb
+              ? null
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () => Get.back(),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.arrow_back_ios_rounded,
+                            color: Theme.of(context).primaryColor,
+                            size: 35.0,
+                          ),
+                          Text(
+                            " Back",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                dialog(context: context, isReload: true);
-              },
-              child: Icon(
-                Icons.refresh,
-                color: Colors.black,
-                size: 35.0,
+                  ],
+                ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: GestureDetector(
+                onTap: () {
+                  dialog(context: context, isReload: true);
+                },
+                child: Icon(
+                  Icons.refresh,
+                  color: Colors.black,
+                  size: 35.0,
+                ),
               ),
             ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 40.0,
-          ),
-          Obx(
-            () => Text(
-              "Steps : $steps",
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30.0,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 40.0,
-          ),
-          Flexible(
-            child: Container(
-              width: size.width,
-              alignment: Alignment.center,
-              padding: EdgeInsets.symmetric(horizontal: !kIsWeb ? 16 : size.width * .3),
-              child: Obx(() => GridView.builder(
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: list.length ~/ matrixLength.value,
-                      mainAxisExtent: !kIsWeb ? null : size.height * .15,
-                      childAspectRatio: !kIsWeb ? 1 : size.height * .15,
-                    ),
-                    itemCount: list.length,
-                    itemBuilder: (context, index) {
-                      return list[index] == ''
-                          ? const Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: FlutterLogo(),
-                            )
-                          : GestureDetector(
-                              onTap: () {
-                                List moveAbleList = [];
-                                int i = list.indexOf('');
-                                try {
-                                  moveAbleList.add(list[i - 1]);
-                                } catch (e) {}
-                                try {
-                                  moveAbleList.add(list[i + matrixLength.value]);
-                                } catch (e) {}
-                                try {
-                                  moveAbleList.add(list[i + 1]);
-                                } catch (e) {}
-                                try {
-                                  moveAbleList.add(list[i - matrixLength.value]);
-                                } catch (e) {}
-                                if (moveAbleList.contains(list[index])) {
-                                  list.insert(i + 1, list[index]);
-                                  list.removeAt(i);
-                                  list.insert(index + 1, '');
-                                  list.removeAt(index);
-                                  steps++;
-                                }
-
-                                if (mainList.toString() == list.toString()) {
-                                  dialog(context: context, isReload: false);
-                                }
-                              },
-                              child: Container(
-                                margin: const EdgeInsets.all(4.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    list[index].toString(),
-                                    style: TextStyle(
-                                      fontSize: !kIsWeb ? 30 : size.height * .03,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                    },
-                  )),
-            ),
-          ),
-        ],
+        body: kIsWeb ? web() : phone(),
       ),
     );
+  }
+
+  Widget phone() {
+    return Column(
+      children: commonWidget(),
+    );
+  }
+
+  Widget web() {
+    return Row(
+      children: commonWidget(),
+    );
+  }
+
+  List<Widget> commonWidget() {
+    return [
+      const SizedBox(
+        height: 40.0,
+      ),
+      Expanded(
+        flex: kIsWeb ? 3 : 1,
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (kIsWeb) ...{
+                FlutterLogo(
+                  size: 100.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "A ${matrixLength.value} x ${matrixLength.value} Puzzle",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+              },
+              Obx(
+                () => Text(
+                  "Steps : $steps",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      const SizedBox(
+        height: 40.0,
+      ),
+      Expanded(
+        flex: kIsWeb ? 7 : 9,
+        child: Obx(() => Container(
+              width: size!.width,
+              height: size!.height,
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: kIsWeb ? ((size!.width * .7) / (matrixLength.value)) : 8),
+              child: GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: list.length ~/ matrixLength.value,
+                  childAspectRatio: 1,
+                  mainAxisExtent: 100,
+                ),
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  return list[index] == ''
+                      ? const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: FlutterLogo(),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            List moveAbleList = [];
+                            int i = list.indexOf('');
+                            try {
+                              moveAbleList.add(list[i - 1]);
+                            } catch (e) {}
+                            try {
+                              moveAbleList.add(list[i + matrixLength.value]);
+                            } catch (e) {}
+                            try {
+                              moveAbleList.add(list[i + 1]);
+                            } catch (e) {}
+                            try {
+                              moveAbleList.add(list[i - matrixLength.value]);
+                            } catch (e) {}
+                            if (moveAbleList.contains(list[index])) {
+                              list.insert(i + 1, list[index]);
+                              list.removeAt(i);
+                              list.insert(index + 1, '');
+                              list.removeAt(index);
+                              steps++;
+                            }
+
+                            if (mainList.toString() == list.toString()) {
+                              dialog(context: context, isReload: false);
+                            }
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            margin: const EdgeInsets.all(4.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            child: Center(
+                              child: Text(
+                                list[index].toString(),
+                                style: TextStyle(
+                                  fontSize: !kIsWeb ? 30 : size!.height * .03,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                },
+              ),
+            )),
+      ),
+      const SizedBox(
+        height: 40.0,
+      ),
+    ];
   }
 
   dialog({required context, required bool isReload}) {
